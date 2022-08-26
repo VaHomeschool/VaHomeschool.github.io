653db8d765d9429d8f7599b53a1981d292fb5194
Redonesgofaster <Russell.Wrightson@vahomeschoolers.org>
2022-08-26 08:23:44 -0400

    Initial Upload
    First upload of established files

---
+++ mapdata.js
@@ -0,0 +1,796 @@
+var simplemaps_statemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#ffffff",
+    background_transparent: "yes",
+    popups: "on_click",
+    
+    //State defaults
+    state_description: "We need your help filling out the info in this county. Email volunteer@vahomeschoolers.org to volunteer!",
+    state_color: "#539175",
+    state_hover_color: "#20bd86",
+    state_url: "https://vahomeschoolers.org/guide",
+    border_size: 1.5,
+    border_color: "#ffffff",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "triangle",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: "8",
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: false,
+    manual_zoom: "yes",
+    back_image: "no",
+    arrow_box: "no",
+    navigation_size: "40",
+    navigation_color: "#f7f7f7",
+    navigation_border_color: "#636363",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    rotate: "0",
+    url_new_tab: "yes",
+    images_directory: "default",
+    import_labels: "no",
+    fade_time: 0.1,
+    link_text: "Our Comprehensive Guide to Homeschooling",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "51001": {
+      name: "Accomack",
+      description: "<a href=\"mailto: della.jordan@accomack.k12.va.us\">Email Proof of progress and Notice of intent here</a><br />"
+    },
+    "51003": {
+      name: "Albemarle",
+      description: "<a href=\"mailto: homeschool@k12albemarle.org\">Email Proof of progress and Notice of Intent here</a><br /> OR Albemarle County Public Schools c/o Homeschool Department 401 McIntire Rd. Suite #333 Charlottesville, VA 22902"
+    },
+    "51005": {
+      name: "Alleghany",
+      description: "Email: khalterman@alleghany.k12.va.us to deliver your Notice of Intent and Proof of Progress. Click to open our comprehensive guide to homeschooling."
+    },
+    "51007": {
+      name: "Amelia"
+    },
+    "51009": {
+      name: "Amherst"
+    },
+    "51011": {
+      name: "Appomattox"
+    },
+    "51013": {
+      name: "Arlington"
+    },
+    "51015": {
+      name: "Augusta"
+    },
+    "51017": {
+      name: "Bath"
+    },
+    "51019": {
+      name: "Bedford"
+    },
+    "51021": {
+      name: "Bland"
+    },
+    "51023": {
+      name: "Botetourt"
+    },
+    "51025": {
+      name: "Brunswick"
+    },
+    "51027": {
+      name: "Buchanan"
+    },
+    "51029": {
+      name: "Buckingham"
+    },
+    "51031": {
+      name: "Campbell"
+    },
+    "51033": {
+      name: "Caroline"
+    },
+    "51035": {
+      name: "Carroll"
+    },
+    "51036": {
+      name: "Charles City"
+    },
+    "51037": {
+      name: "Charlotte"
+    },
+    "51041": {
+      name: "Chesterfield"
+    },
+    "51043": {
+      name: "Clarke"
+    },
+    "51045": {
+      name: "Craig"
+    },
+    "51047": {
+      name: "Culpeper"
+    },
+    "51049": {
+      name: "Cumberland"
+    },
+    "51051": {
+      name: "Dickenson"
+    },
+    "51053": {
+      name: "Dinwiddie"
+    },
+    "51057": {
+      name: "Essex"
+    },
+    "51059": {
+      name: "Fairfax"
+    },
+    "51061": {
+      name: "Fauquier"
+    },
+    "51063": {
+      name: "Floyd"
+    },
+    "51065": {
+      name: "Fluvanna"
+    },
+    "51067": {
+      name: "Franklin"
+    },
+    "51069": {
+      name: "Frederick"
+    },
+    "51071": {
+      name: "Giles"
+    },
+    "51073": {
+      name: "Gloucester"
+    },
+    "51075": {
+      name: "Goochland"
+    },
+    "51077": {
+      name: "Grayson"
+    },
+    "51079": {
+      name: "Greene"
+    },
+    "51081": {
+      name: "Greensville"
+    },
+    "51083": {
+      name: "Halifax"
+    },
+    "51085": {
+      name: "Hanover"
+    },
+    "51087": {
+      name: "Henrico"
+    },
+    "51089": {
+      name: "Henry"
+    },
+    "51091": {
+      name: "Highland"
+    },
+    "51093": {
+      name: "Isle Of Wight"
+    },
+    "51095": {
+      name: "James City"
+    },
+    "51097": {
+      name: "King And Queen"
+    },
+    "51099": {
+      name: "King George"
+    },
+    "51101": {
+      name: "King William"
+    },
+    "51103": {
+      name: "Lancaster"
+    },
+    "51105": {
+      name: "Lee"
+    },
+    "51107": {
+      name: "Loudoun"
+    },
+    "51109": {
+      name: "Louisa"
+    },
+    "51111": {
+      name: "Lunenburg"
+    },
+    "51113": {
+      name: "Madison"
+    },
+    "51115": {
+      name: "Mathews"
+    },
+    "51117": {
+      name: "Mecklenburg"
+    },
+    "51119": {
+      name: "Middlesex"
+    },
+    "51121": {
+      name: "Montgomery"
+    },
+    "51125": {
+      name: "Nelson"
+    },
+    "51127": {
+      name: "New Kent"
+    },
+    "51131": {
+      name: "Northampton"
+    },
+    "51133": {
+      name: "Northumberland"
+    },
+    "51135": {
+      name: "Nottoway"
+    },
+    "51137": {
+      name: "Orange"
+    },
+    "51139": {
+      name: "Page"
+    },
+    "51141": {
+      name: "Patrick"
+    },
+    "51143": {
+      name: "Pittsylvania"
+    },
+    "51145": {
+      name: "Powhatan"
+    },
+    "51147": {
+      name: "Prince Edward"
+    },
+    "51149": {
+      name: "Prince George"
+    },
+    "51153": {
+      name: "Prince William"
+    },
+    "51155": {
+      name: "Pulaski"
+    },
+    "51157": {
+      name: "Rappahannock"
+    },
+    "51159": {
+      name: "Richmond"
+    },
+    "51161": {
+      name: "Roanoke"
+    },
+    "51163": {
+      name: "Rockbridge"
+    },
+    "51165": {
+      name: "Rockingham"
+    },
+    "51167": {
+      name: "Russell"
+    },
+    "51169": {
+      name: "Scott"
+    },
+    "51171": {
+      name: "Shenandoah"
+    },
+    "51173": {
+      name: "Smyth"
+    },
+    "51175": {
+      name: "Southampton"
+    },
+    "51177": {
+      name: "Spotsylvania",
+      description: "<a href=\"Mailto: Homeschool@spotsylvania.k12.va.us\">Email Proof of progress and Notice of intent here</a> Mailing Address: 8020 River Stone Drive Fredericksburg, VA 22407"
+    },
+    "51179": {
+      name: "Stafford"
+    },
+    "51181": {
+      name: "Surry"
+    },
+    "51183": {
+      name: "Sussex"
+    },
+    "51185": {
+      name: "Tazewell"
+    },
+    "51187": {
+      name: "Warren"
+    },
+    "51191": {
+      name: "Washington"
+    },
+    "51193": {
+      name: "Westmoreland"
+    },
+    "51195": {
+      name: "Wise"
+    },
+    "51197": {
+      name: "Wythe"
+    },
+    "51199": {
+      name: "York"
+    },
+    "51510": {
+      name: "Alexandria",
+      description: "<a href=\"mailto: Homeinstruction@acps.k12.va.us\">Email Proof of progress and Notice of intent here</a><br /> OR MAIL: Attn: Dr. Marcia Jackson Department of Student Services 1340 Braddock Place, 5th Floor, Alexandria, Va 22314."
+    },
+    "51520": {
+      name: "Bristol"
+    },
+    "51530": {
+      name: "Buena Vista"
+    },
+    "51540": {
+      name: "Charlottesville"
+    },
+    "51550": {
+      name: "Chesapeake"
+    },
+    "51570": {
+      name: "Colonial Heights"
+    },
+    "51580": {
+      name: "Covington"
+    },
+    "51590": {
+      name: "Danville"
+    },
+    "51595": {
+      name: "Emporia"
+    },
+    "51600": {
+      name: "Fairfax"
+    },
+    "51610": {
+      name: "Falls Church"
+    },
+    "51620": {
+      name: "Franklin"
+    },
+    "51630": {
+      name: "Fredericksburg"
+    },
+    "51640": {
+      name: "Galax"
+    },
+    "51650": {
+      name: "Hampton"
+    },
+    "51660": {
+      name: "Harrisonburg"
+    },
+    "51670": {
+      name: "Hopewell"
+    },
+    "51678": {
+      name: "Lexington"
+    },
+    "51680": {
+      name: "Lynchburg"
+    },
+    "51683": {
+      name: "Manassas"
+    },
+    "51685": {
+      name: "Manassas Park"
+    },
+    "51690": {
+      name: "Martinsville"
+    },
+    "51700": {
+      name: "Newport News"
+    },
+    "51710": {
+      name: "Norfolk"
+    },
+    "51720": {
+      name: "Norton"
+    },
+    "51730": {
+      name: "Petersburg"
+    },
+    "51735": {
+      name: "Poquoson"
+    },
+    "51740": {
+      name: "Portsmouth"
+    },
+    "51750": {
+      name: "Radford"
+    },
+    "51760": {
+      name: "Richmond"
+    },
+    "51770": {
+      name: "Roanoke"
+    },
+    "51775": {
+      name: "Salem"
+    },
+    "51790": {
+      name: "Staunton"
+    },
+    "51800": {
+      name: "Suffolk"
+    },
+    "51810": {
+      name: "Virginia Beach"
+    },
+    "51820": {
+      name: "Waynesboro"
+    },
+    "51830": {
+      name: "Williamsburg"
+    },
+    "51840": {
+      name: "Winchester"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 38.255,
+      lng: -77.542,
+      name: "Fredericksburg Area Homeschool Association (FaHa)",
+      color: "red",
+      description: "Regional, Eclectic, and Secular, Homeschool Support",
+      url: "https://www.fahahome.com/",
+      size: "10",
+      type: "circle"
+    },
+    "1": {
+      lat: 38.284,
+      lng: -77.512,
+      name: "Fredericksburg Area Young Musicians (FAYM)",
+      color: "red",
+      description: "Homeschool friendly music education. Band, chorus, theory, and more.",
+      url: "https://www.faym-music.com/",
+      size: "10",
+      type: "circle"
+    },
+    "2": {
+      lat: 38.199,
+      lng: -77.534,
+      name: "YMCA: Ron Rosner",
+      color: "cyan",
+      description: "Homeschool Fitness. Homeschool PE classes",
+      url: "https://family-ymca.org/programs/youth-sports/homeschool-fitness/",
+      size: "10",
+      type: "star"
+    },
+    "3": {
+      lat: 38.261,
+      lng: -77.438,
+      name: "Rapp Rocks Climbing Gym",
+      color: "red",
+      description: "Climbing gym with discounted days for homeschoolers during the school year.",
+      url: "https://www.rapprocksclimbing.com/?fbclid=IwAR1K-Gt-bfh1sUOU_i3K1sO2txAt77kqiyPvGEkS48wqqyyLieY41pwgON8",
+      size: "10",
+      type: "star"
+    },
+    "4": {
+      lat: 38.303,
+      lng: -77.513,
+      name: "SkyZone Fredericksburg",
+      color: "red",
+      description: "Trampoline Park with homeschool membership pricing during the school year",
+      url: "https://www.skyzone.com/fredericksburg/contact",
+      size: "10",
+      type: "star"
+    },
+    "5": {
+      lat: 38.316,
+      lng: -77.456,
+      name: "YMCA: Massad",
+      color: "cyan",
+      description: "Homeschool Fitness. Homeschool PE classes",
+      url: "https://family-ymca.org/programs/youth-sports/homeschool-fitness/",
+      size: "10",
+      type: "star"
+    },
+    "6": {
+      lat: 38.255,
+      lng: -77.162,
+      name: "YMCA: King George",
+      color: "cyan",
+      description: "Homeschool Fitness. Homeschool PE classes",
+      url: "https://family-ymca.org/programs/youth-sports/homeschool-fitness/",
+      size: "10",
+      type: "star"
+    },
+    "7": {
+      lat: 38.022,
+      lng: -77.521,
+      name: "YMCA: Caroline",
+      color: "cyan",
+      description: "Homeschool Fitness. Homeschool PE classes",
+      url: "https://family-ymca.org/programs/youth-sports/homeschool-fitness/",
+      size: "10",
+      type: "star"
+    },
+    "8": {
+      lat: 38.201,
+      lng: -77.587,
+      name: "Reach Homeschool",
+      color: "red",
+      description: "Homeschool classes, trips, social meetings and more",
+      size: "10",
+      type: "circle",
+      url: "https://reachhomeschoolgroupva.org/"
+    },
+    "9": {
+      lat: 38.294,
+      lng: -77.529,
+      name: "Grace Co-op",
+      color: "cyan",
+      description: "Since 2006, Grace Homeschool Co-op (GHC) has been a ministry of Grace Church of Fredericksburg, Virginia. Our purpose is to unify and serve the unique needs of Grace Church’s homeschooling families and those in the Bible-believing Christian community.",
+      url: "https://www.homeschool-life.com/va/gracechurchcoop/",
+      size: "10",
+      type: "circle"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {
+    "0": {
+      states: [
+        "51003",
+        "51007",
+        "51033",
+        "51036",
+        "51041",
+        "51053",
+        "51065",
+        "51075",
+        "51079",
+        "51085",
+        "51087",
+        "51101",
+        "51109",
+        "51113",
+        "51125",
+        "51127",
+        "51137",
+        "51145",
+        "51149",
+        "51540",
+        "51570",
+        "51670",
+        "51730",
+        "51760"
+      ],
+      name: "Central Virginia",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "1": {
+      states: [
+        "51009",
+        "51011",
+        "51019",
+        "51023",
+        "51031",
+        "51045",
+        "51063",
+        "51067",
+        "51071",
+        "51121",
+        "51155",
+        "51161",
+        "51680",
+        "51750",
+        "51770",
+        "51775"
+      ],
+      name: "West Central",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "2": {
+      states: [
+        "51025",
+        "51029",
+        "51037",
+        "51049",
+        "51081",
+        "51083",
+        "51089",
+        "51111",
+        "51117",
+        "51135",
+        "51141",
+        "51143",
+        "51147",
+        "51175",
+        "51183",
+        "51590",
+        "51620",
+        "51690",
+        "51595"
+      ],
+      name: "Southside",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "3": {
+      states: [
+        "51073",
+        "51093",
+        "51095",
+        "51115",
+        "51181",
+        "51199",
+        "51550",
+        "51650",
+        "51700",
+        "51710",
+        "51735",
+        "51800",
+        "51810",
+        "51830",
+        "51740"
+      ],
+      name: "Hampton Roads",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "4": {
+      states: [
+        "51001",
+        "51057",
+        "51097",
+        "51103",
+        "51119",
+        "51131",
+        "51133",
+        "51159",
+        "51193"
+      ],
+      name: "Eastern",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "5": {
+      states: [
+        "51021",
+        "51027",
+        "51035",
+        "51051",
+        "51077",
+        "51105",
+        "51167",
+        "51169",
+        "51173",
+        "51185",
+        "51191",
+        "51195",
+        "51197",
+        "51520",
+        "51640",
+        "51720"
+      ],
+      name: "Southwest",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "6": {
+      states: [
+        "51013",
+        "51043",
+        "51047",
+        "51049",
+        "51061",
+        "51099",
+        "51107",
+        "51153",
+        "51157",
+        "51177",
+        "51179",
+        "51187",
+        "51510",
+        "51600",
+        "51610",
+        "51630",
+        "51683",
+        "51685",
+        "51059"
+      ],
+      name: "Northern",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    },
+    "7": {
+      states: [
+        "51005",
+        "51015",
+        "51017",
+        "51069",
+        "51091",
+        "51139",
+        "51163",
+        "51165",
+        "51171",
+        "51530",
+        "51580",
+        "51660",
+        "51678",
+        "51790",
+        "51820",
+        "51840"
+      ],
+      name: "Valley",
+      color: "#539175",
+      hover_color: "#20bd86",
+      zoomable: "yes",
+      description: "Click to zoom in and find your county, who to send your Notice of Intent and Proof of progress to, as well as any homeschool opportunities in your area."
+    }
+  }
+};
