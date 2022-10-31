54384ab93d9c52e0c7755b61439c871408f8f56e
VaHomeschool <112176986+VaHomeschool@users.noreply.github.com>
2022-10-31 15:01:26 -0400

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -89,7 +89,14 @@ var simplemaps_statemap_mapdata={
     },
     "51007": {
       name: "Amelia",
-      description: "https://www.canva.com/design/DAFQdbssH9Q/view"
+      description: "<div style="position: relative; width: 100%; height: 0; padding-top: 100.0000%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFQdbssH9Q&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFQdbssH9Q&#x2F;view?utm_content=DAFQdbssH9Q&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">sample</a> by Russell Wrightson"
     },
     "51009": {
       name: "Amherst"
