487e94ee288060fe6054588a73b4ea9d0d0ccc86
VaHomeschool <112176986+VaHomeschool@users.noreply.github.com>
2022-11-13 10:46:52 -0500

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -89,7 +89,7 @@ var simplemaps_statemap_mapdata={
     },
     "51007": {
       name: "Amelia",
-      description: "<div style=\"position: relative; width: 100%; height: 0; padding-top: 100.0000%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;\">  <iframe loading=\"lazy\" style=\"position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;\"    src=\"https://www.canva.com/design/DAFQdbssH9Q/view?embed\" allowfullscreen=\"allowfullscreen\" allow=\"fullscreen\">  </iframe></div><a href=\"https://www.canva.com/design/DAFQdbssH9Q/view\" target=\"_blank\" rel=\"noopener\">sample</a> by Russell Wrightson",
+      description: "<div style=\"position: relative; width: 100%; height: 0; padding-top: 100.0000%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;\">  <iframe loading=\"lazy\" style=\"position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;\"    src=\"https://www.canva.com/design/DAFQdbssH9Q/view?embed\" allowfullscreen=\"allowfullscreen\" allow=\"fullscreen\">,
     },
     "51009": {
       name: "Amherst"
